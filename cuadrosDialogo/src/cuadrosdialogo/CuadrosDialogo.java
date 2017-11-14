6d74329a1a2535f95e3cd1a3c061e92e63b3dae6
Blueblee <33637132+Blueblee@users.noreply.github.com>
2017-11-13 18:27:36 -0600

    Add files via upload

---
+++ cuadrosDialogo/src/cuadrosdialogo/CuadrosDialogo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuadrosdialogo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author andor
+ */
+public class CuadrosDialogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Definir variable de entrada
+        String cumpleaños, nombre;
+        nombre = JOptionPane.showInputDialog("Inserte su nombre");
+        JOptionPane.showMessageDialog(null, "Hola " + nombre + "\n" + "   uwu" +"\n"+ "Atte. Andrea & Emily <3");
+        //Cumpleaños
+        
+
+    }
+    
+}
